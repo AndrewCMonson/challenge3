338dea3268a47ee897cd26d9dc14f0a932b4fba3
Andrew Monson <139920681+AndrewCMonson@users.noreply.github.com>
2023-09-28 20:07:02 -0400

    minor formatting changes to script.js and height change to main in style.css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Declare the button const to grab button from DOM
 const button = document.getElementById('gen-button');
+
 // Global declaration of character set consts
 const upperCharSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCharSet = 'abcdefghijklmnopqrstuvwxyz';
@@ -14,19 +15,19 @@ const randomChar = (string) => {
 // Event listener that waits for button click and starts chain of prompts. Prompt inputs are stored as individual const to be used later
 button.addEventListener('click', e => {
     
-    const pwordLengthInput = prompt("Select password length", 20);
+    const pwordLengthInput = prompt('Select password length', 20);
     if(pwordLengthInput < 8 || pwordLengthInput > 128 || isNaN(pwordLengthInput)) {
-        alert("please select a number between 8 and 128");
+        alert('please select a number between 8 and 128');
         return;
     } 
-    const uppercase = confirm("Inlcude uppercase letters? OK for yes, cancel for no");
-    const lowercase = confirm("Include lowercase letters? OK for yes, cancel for no");
-    const numeric = confirm("Include numbers? OK for yes, cancel for no");
-    const specChars = confirm("Include special characters? OK for yes,cancel for no");
+    const uppercase = confirm('Inlcude uppercase letters? OK for yes, cancel for no');
+    const lowercase = confirm('Include lowercase letters? OK for yes, cancel for no');
+    const numeric = confirm('Include numbers? OK for yes, cancel for no');
+    const specChars = confirm('Include special characters? OK for yes,cancel for no');
 
     // verifies that at least one character type was selected and if not, returns alert to user
     if(!uppercase && !lowercase && !numeric && !specChars){
-        alert("You must select at least one character type");
+        alert('You must select at least one character type');
         return;
     }
     
@@ -35,7 +36,7 @@ button.addEventListener('click', e => {
 
     alert(`Characters selected: ${validation}`);
 
-    // uses ternary operators to determine if prompts were answered "true or false" and pushes respective character set into charSet variable to create master character list to be iterated through later
+    // uses ternary operators to determine if prompts were answered 'true or false' and pushes respective character set into charSet variable to create master character list to be iterated through later
     const charSet = `${uppercase? upperCharSet : ''}${lowercase? lowerCharSet : ''}${numeric? numericCharSet : '' }${specChars? specCharSet : ''}`;
 
     // blank variable to store final password
@@ -47,16 +48,14 @@ button.addEventListener('click', e => {
     }
 
     // alerts the user to check the screen for generated password
-    alert("Password Generated, see page for password");
+    alert('Password Generated, see page for password');
 
     // identifies generated password HTML element and replaces it's text with the password variable
     document.getElementById('generated-password').innerHTML=password;
 
 })
 
-
 // allows user to copy text to clipboard for us in different application
-
 const copyButton = document.getElementById('copy-button');
 
 copyButton.addEventListener('click', e => {
