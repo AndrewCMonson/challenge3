471e5d2205c905d78d8961d5679241ab6af9e184
Andrew Monson <andrewmonsondev@gmail.com>
2023-09-29 11:45:31 -0400

    capitalized 'Cancel' on lines 30-33

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ button.addEventListener('click', e => {
         return;
     } 
     
-    const uppercase = confirm('Include uppercase letters? OK for yes, cancel for no');
-    const lowercase = confirm('Include lowercase letters? OK for yes, cancel for no');
-    const numeric = confirm('Include numbers? OK for yes, cancel for no');
-    const specChars = confirm('Include special characters? OK for yes, cancel for no');
+    const uppercase = confirm('Include uppercase letters? OK for yes, Cancel for no');
+    const lowercase = confirm('Include lowercase letters? OK for yes, Cancel for no');
+    const numeric = confirm('Include numbers? OK for yes, Cancel for no');
+    const specChars = confirm('Include special characters? OK for yes, Cancel for no');
 
     // verifies that at least one character type was selected and if not, returns alert to user
     if(!uppercase && !lowercase && !numeric && !specChars){
